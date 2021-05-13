6760e544ff179c81c31639d73c2ce8de15a58416
Aniruddha2951 <83918201+Aniruddha2951@users.noreply.github.com>
2021-05-13 15:04:31 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,91 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from tkinter import *
+import pyttsx3 as pp
+import speech_recognition as s
+import threading
+
+engine = pp.init()
+
+voices = engine.getProperty('voices')
+print(voices)
+
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(word):
+    engine.say(word)
+    engine.runAndWait()
+
+
+# pyttsx3
+bot = ChatBot("My Bot")
+
+convo = [
+    'hello',
+    'hi there !',
+    'what is your name ?',
+    'My name is Bot , i am created by Durgesh',
+    'how are you ?',
+    'I am doing great these days',
+    'thank you',
+    'In which city you live ?',
+    'I live in lucknow',
+    'In which language you talk?',
+    ' I mostly talk in english'
+]
+
+trainer = ListTrainer(bot)
+
+# now training the bot with the help of trainer
+
+trainer.train(convo)
+
+# answer = bot.get_response("what is your name?")
+# print(answer)
+
+# print("Talk to bot ")
+# while True:
+#     query = input()
+#     if query == 'exit':
+#         break
+#     answer = bot.get_response(query)
+#     print("bot : ", answer)
+
+main = Tk()
+
+main.geometry("500x650")
+
+main.title("My Chat bot")
+img = PhotoImage(file="bot1.png")
+
+photoL = Label(main, image=img)
+
+photoL.pack(pady=5)
+
+
+# takey query : it takes audio as input from user and convert it to string..
+
+def takeQuery():
+    sr = s.Recognizer()
+    sr.pause_threshold = 1
+    print("your bot is listening try to speak")
+    with s.Microphone() as m:
+        try:
+            audio = sr.listen(m)
+            query = sr.recognize_google(audio, language='eng-in')
+            print(query)
+            textF.delete(0, END)
+            textF.insert(0, query)
+            ask_from_bot()
+        except Exception as e:
+            print(e)
+            print("not recognized")
+
+
+def ask_from_bot():
+    query = textF.get()
+    answer_from_bot = bot.get_response(query)
+    msgs.insert(END, "you : " + query)
+    print(type(answer_from_bot))
+    msgs.insert(END, "bot : " + str(answer_from_bot))
